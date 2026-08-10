cd7d43c11fa14831b9e29fd34bcab374ec7dfe89
Civi <19486398+angribot@users.noreply.github.com>
2026-08-10 18:43:46 +0800

    refactor: clarify sing-box constants package (#9)

--- tools/srs-compile/main.go
+++ tools/srs-compile/main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 
 	"github.com/sagernet/sing-box/common/srs"
-	C "github.com/sagernet/sing-box/constant"
+	"github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/option"
 	"github.com/sagernet/sing/common/json"
 )
@@ -58,16 +58,16 @@ func compileRuleSet(sourcePath string, outputPath string) error {
 
 // downgradeRuleSetVersion mirrors the sing-box compiler's compatibility behavior.
 func downgradeRuleSetVersion(version uint8, ruleSet option.PlainRuleSet) uint8 {
-	if version == C.RuleSetVersion4 && !hasHeadlessRule(ruleSet.Rules, func(rule option.DefaultHeadlessRule) bool {
+	if version == constant.RuleSetVersion4 && !hasHeadlessRule(ruleSet.Rules, func(rule option.DefaultHeadlessRule) bool {
 		return rule.NetworkInterfaceAddress != nil && rule.NetworkInterfaceAddress.Size() > 0 ||
 			len(rule.DefaultInterfaceAddress) > 0
 	}) {
-		version = C.RuleSetVersion3
+		version = constant.RuleSetVersion3
 	}
-	if version == C.RuleSetVersion3 && !hasHeadlessRule(ruleSet.Rules, func(rule option.DefaultHeadlessRule) bool {
+	if version == constant.RuleSetVersion3 && !hasHeadlessRule(ruleSet.Rules, func(rule option.DefaultHeadlessRule) bool {
 		return len(rule.NetworkType) > 0 || rule.NetworkIsExpensive || rule.NetworkIsConstrained
 	}) {
-		version = C.RuleSetVersion2
+		version = constant.RuleSetVersion2
 	}
 	return version
 }
@@ -75,11 +75,11 @@ func downgradeRuleSetVersion(version uint8, ruleSet option.PlainRuleSet) uint8 {
 func hasHeadlessRule(rules []option.HeadlessRule, condition func(rule option.DefaultHeadlessRule) bool) bool {
 	for _, rule := range rules {
 		switch rule.Type {
-		case C.RuleTypeDefault:
+		case constant.RuleTypeDefault:
 			if condition(rule.DefaultOptions) {
 				return true
 			}
-		case C.RuleTypeLogical:
+		case constant.RuleTypeLogical:
 			if hasHeadlessRule(rule.LogicalOptions.Rules, condition) {
 				return true
 			}
